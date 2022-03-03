c87c75b01c581aa62a1db61b73a3678e32a3b5c3
Cheng-Cheng Yang <chengchengyang0416@gmail.com>
2022-03-03 09:32:27 +0800

    append ABC orientation and time list to SampledPose

--- Modules/DataProcessor.py
+++ Modules/DataProcessor.py
@@ -3,7 +3,7 @@ from scipy.spatial.transform import Rotation as R
 class CDataProcessor:
 	def GetSampledPositionAndOrientation( self, Pose, SamplingInterval ):
 		# dictionary of sampled position and orientation
-		SampledPose = { 'X' : [], 'Y' : [], 'Z' : [], 'OrientationXX' : [], 'OrientationXY' : [], 'OrientationXZ' : [], 'OrientationYX' : [], 'OrientationYY' : [], 'OrientationYZ' : [], 'OrientationZX' : [], 'OrientationZY' : [], 'OrientationZZ' : [] }
+		SampledPose = { 'X' : [], 'Y' : [], 'Z' : [], 'A' : [], 'B' : [], 'C' : [], 'OrientationXX' : [], 'OrientationXY' : [], 'OrientationXZ' : [], 'OrientationYX' : [], 'OrientationYY' : [], 'OrientationYZ' : [], 'OrientationZX' : [], 'OrientationZY' : [], 'OrientationZZ' : [], 'Time' : [] }
 
 		# get sampled position and orientation
 		for i in range( 0, len( Pose[ 0 ] ), SamplingInterval ):
@@ -11,6 +11,11 @@ class CDataProcessor:
 			SampledPose[ 'X' ].append( Pose[ 0 ][ i ] )
 			SampledPose[ 'Y' ].append( Pose[ 1 ][ i ] )
 			SampledPose[ 'Z' ].append( Pose[ 2 ][ i ] )
+
+			# get orientation by ABC
+			SampledPose[ 'A' ].append( Pose[ 3 ][ i ] )
+			SampledPose[ 'B' ].append( Pose[ 4 ][ i ] )
+			SampledPose[ 'C' ].append( Pose[ 5 ][ i ] )
 
 			# get orientation by rotation matrix
 			r = R.from_euler( 'zyx', [ Pose[ 3 ][ i ], Pose[ 4 ][ i ], Pose[ 5 ][ i ] ], degrees = True )
@@ -23,5 +28,8 @@ class CDataProcessor:
 			SampledPose[ 'OrientationZX' ].append( r.as_matrix()[ 0 ][ 2 ] )
 			SampledPose[ 'OrientationZY' ].append( r.as_matrix()[ 1 ][ 2 ] )
 			SampledPose[ 'OrientationZZ' ].append( r.as_matrix()[ 2 ][ 2 ] )
+
+			# produce time list
+			SampledPose[ 'Time' ].append( i * 0.003 )
 
 		return SampledPose
